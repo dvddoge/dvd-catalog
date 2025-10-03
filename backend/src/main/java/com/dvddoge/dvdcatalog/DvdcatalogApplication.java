483473e14f53d76eca00dc98e0c34eb166a890b3
Deyvid Jerônimo <davidjmacedo@hotmail.com>
2025-10-03 10:30:40 -0300

    backend initialized

---
+++ backend/src/main/java/com/dvddoge/dvdcatalog/DvdcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.dvddoge.dvdcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdcatalogApplication.class, args);
+	}
+
+}
