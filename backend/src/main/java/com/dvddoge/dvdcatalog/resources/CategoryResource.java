c5af4f43ca903e7e34ef98dca97914a8c705d3f3
Deyvid Jerônimo <davidjmacedo@hotmail.com>
2025-10-03 13:19:36 -0300

    Create category resource

---
+++ backend/src/main/java/com/dvddoge/dvdcatalog/resources/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.dvddoge.dvdcatalog.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dvddoge.dvdcatalog.entities.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();   
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electronics"));
+        return ResponseEntity.ok().body(list);
+    }
+}
