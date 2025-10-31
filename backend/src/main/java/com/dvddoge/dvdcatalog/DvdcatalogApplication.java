441e84df330e7992ca0695ed69ce83b354ec3b58
Deyvid Jerônimo <davidjmacedo@hotmail.com>
2025-10-31 11:27:35 -0300

    feat: refactor busca paginada de categoria e produto
    - Refatorar a busca paginada pelo uso do `Pageable` do spring

--- backend/src/main/java/com/dvddoge/dvdcatalog/DvdcatalogApplication.java
+++ backend/src/main/java/com/dvddoge/dvdcatalog/DvdcatalogApplication.java
@@ -9,5 +9,4 @@ public class DvdcatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DvdcatalogApplication.class, args);
 	}
-
 }
--- backend/src/main/java/com/dvddoge/dvdcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/dvddoge/dvdcatalog/resources/CategoryResource.java
@@ -5,14 +5,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dvddoge.dvdcatalog.dto.CategoryDTO;
 import com.dvddoge.dvdcatalog.services.CategoryService;
-import com.dvddoge.dvdcatalog.services.exceptions.ResourceNotFoundException;
 
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -31,15 +28,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
-
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-
-        Page<CategoryDTO> categorias = service.findAllPaged(pageRequest);
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> categorias = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(categorias);
     }
 
